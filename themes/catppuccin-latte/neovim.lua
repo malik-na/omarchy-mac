a695a79b97595f4130716366510c4cdeeb342d83
Ryan Hughes <ryan@heyoodle.com>
2025-10-05 11:18:40 -0400

    Add auto-changing colors for Neovim (#1712)
    * Add MVP variant of nvim switcher
    
    * Update nvim configs
    
    * Remove config
    
    * Add migration for the new live themeing
    
    * Fix plugin
    
    * Remove theme
    
    * Update to actually work
    
    * Make it faster
    
    * Fix quirks
    
    ---------
    
    Co-authored-by: David Heinemeier Hansson <david@hey.com>

--- themes/catppuccin-latte/neovim.lua
+++ themes/catppuccin-latte/neovim.lua
@@ -1,5 +1,13 @@
 return {
 	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "latte",
+		},
+	},
+	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "tokyonight",
