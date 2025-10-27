d70894e9be4605716cd1094ff08f391c1e42c484
David Heinemeier Hansson <david@hey.com>
2025-10-27 14:51:52 +0100

    Already in mise-work

--- install/packaging/nvim.sh
+++ install/packaging/nvim.sh
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-#!/bin/bash
-omarchy-lazyvim-setup
-=======
 # Node is needed for neovim treesitter
 mise use -g node@latest
 
 # Includes lazyvim and the themes
 omarchy-nvim-setup
->>>>>>> bba2ade7 (Ensure node is installed for use by treesitter in neovim)
