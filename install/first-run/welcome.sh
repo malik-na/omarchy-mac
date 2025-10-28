ba24f009239d2a0f8090a5659fff48383775ff36
David Heinemeier Hansson <david@hey.com>
2025-10-28 18:19:28 +0100

    Moved

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-notify-send "    Update System" "When you have internet, click to update the system." -u critical
 notify-send "    Learn Keybindings" "Super + K for cheatsheet.\nSuper + Space for application launcher.\nSuper + Alt + Space for Omarchy Menu." -u critical
