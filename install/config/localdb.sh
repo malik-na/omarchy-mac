8a54d39618750d0d2aef4986e641cb84783a260f
Naeem Malik <malik2015naeem@gmail.com>
2026-01-05 04:26:11 +0530

    Add check for updatedb command in localdb script to prevent errors

--- install/config/localdb.sh
+++ install/config/localdb.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Update localdb so that locate will find everything installed
-sudo updatedb
+if command -v updatedb &>/dev/null; then
+  sudo updatedb
+else
+  echo "[SKIP] updatedb not found (mlocate/plocate not installed)"
+fi
