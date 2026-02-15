578047620e5040328e806605396a230d69bc96de
Naeem Malik <malik2015naeem@gmail.com>
2026-02-15 11:45:54 +0530

    undid comments made in hyprland config to suit v 51.x and fixed ghostty config errors

--- migrations/1763633307.sh
+++ migrations/1763633307.sh
@@ -1,5 +1,5 @@
 echo "Add 100-line split resizing keybindings to Ghostty"
 
 if ! grep -q "resize_split:down,100" ~/.config/ghostty/config; then
-  sed -i "/keybind = control+insert=copy_to_clipboard/a\keybind = super+control+shift+alt+arrow_down=resize_split:down,100\nkeybind = super+control+shift+alt+arrow_up=resize_split:up,100\nkeybind = super+control+shift+alt+arrow_left=resize_split:left,100\nkeybind = super+control+shift+alt+arrow_right=resize_split:right,100" ~/.config/ghostty/config
+  sed -i "/keybind = control+insert=copy_to_clipboard/a\keybind = super+control+shift+alt+down=resize_split:down,100\nkeybind = super+control+shift+alt+up=resize_split:up,100\nkeybind = super+control+shift+alt+left=resize_split:left,100\nkeybind = super+control+shift+alt+right=resize_split:right,100" ~/.config/ghostty/config
 fi
