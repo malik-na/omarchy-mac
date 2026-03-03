7401414e976f05f6739f13145d61209af85a816a
eightscrow <183341520+eightscrow@users.noreply.github.com>
2026-03-03 09:39:06 +0200

    fix(first-run): restore correct welcome message with heart emoji

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Update System notification is sent by wifi.sh (either with or without internet variant)
 # Do not send it here to avoid duplicate notifications
-notify-send "👋  Welcome to Omarchy" "You're in for a great computing adventure. Have fun!" -t 30000
+notify-send "👋  Welcome to Omarchy Mac Fedora" "You're in for a great computing adventure. Have fun ❤️" -t 30000
